9e518b9e852127181281e85f4026b01bc3a7e5e9
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-05-17 09:33:06 -0600

    add last change

--- src/Llevo.jsx
+++ src/Llevo.jsx
@@ -5,7 +5,7 @@ export const Llevo = () => {
     <div className="container">
       <h3 className='ms-5 mt-4' style={{ color: 'black', fontSize: "50px", fontWeight: "bold", textAlign: "center" }}>Lo que me llevo de este Primer Ciclo</h3>
       <div className="llevo-content">
-        <ul>
+        <ul className='ms-5'> 
           <li>Relaciones significativas con compañeros de clase y profesores, que han enriquecido mi experiencia educativa.</li>
           <li>Mejoras en mis métodos de estudio, incluyendo técnicas más efectivas para la gestión del tiempo y la organización.</li>
           <li>Refinamiento de mis objetivos académicos y profesionales, con una mayor claridad sobre lo que quiero lograr en el futuro.</li>
